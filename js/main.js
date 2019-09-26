7804381c4903dec5605df626ec4ee9073ecd6530
Булат <booolat@gmail.com>
2019-09-26 11:32:20 +0300

    Мокапы

--- js/main.js
+++ js/main.js
@@ -1,24 +1,50 @@
 'use strict';
 
-var mockData = {
-  url: 'photos/5.jpg',
-  description: 'Описание фотографии',
-  likes: 10,
-  comments: 'Шляпа полная'
-};
+var photoContainer = document.querySelector('.pictures');
 
 var mockTemplate = document.querySelector('#picture').content;
 
-var photoContainer = document.querySelector('.pictures');
+var fragment = document.createDocumentFragment();
+
+var commentTexts = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+
+var commentAuthors = ['Порфирий', 'Цифровой олень', 'Куртка Бэйна', 'Владимир Ясно Солнышко', 'Наталья Орейро', 'Пушистый друг', 'Последний абориген Полинезии', 'Коля, просто Коля', 'Мама Стифлера'];
+
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+var createMock = function () {
+  var mock = {
+    url: 'photos/' + getRandomNumber(1, 25) + '.jpg',
+    description: 'Описание фотографии',
+    likes: getRandomNumber(15, 200),
+    comments: [{avatar: 'img/avatar-' + getRandomNumber(1, 6) + '.svg', message: commentTexts[getRandomNumber(0, 5)], name: commentAuthors[getRandomNumber(0, 8)]}]
+  };
+  return mock;
+};
+
+var createMockArray = function () {
+  var mockArray = [];
+  for (var i = 0; i < 25; i++) {
+    mockArray.push(createMock());
+  }
+  return mockArray;
+};
+
+var mockArray = createMockArray();
 
 for (var i = 0; i < 25; i++) {
   var photoMock = mockTemplate.cloneNode(true);
 
-  photoMock.querySelector('.picture__img').setAttribute('src', mockData.url);
-  photoMock.querySelector('.picture__likes').textContent = mockData.likes;
-  photoMock.querySelector('.picture__comments').textContent = mockData.comments;
+  photoMock.querySelector('.picture__img').setAttribute('src', mockArray[i].url);
+  photoMock.querySelector('.picture__likes').textContent = mockArray[i].likes;
+  photoMock.querySelector('.picture__comments').textContent = mockArray[i].comments;
 
-  var fragment = photoMock.createDocumentFragment();
-
-  photoContainer.appendChild(fragment);
+  fragment.appendChild(photoMock);
 }
+
+photoContainer.appendChild(fragment);
+// debugger
