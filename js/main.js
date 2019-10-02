7d5d98faebd3a8ea67f15d4ebe10c61395fdbbc4
Булат <booolat@gmail.com>
2019-10-02 19:48:07 +0300

    Задание по событиям

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 'use strict';
 
 var photoContainer = document.querySelector('.pictures');
-
 var mockTemplate = document.querySelector('#picture').content;
-
 var fragment = document.createDocumentFragment();
-
-var MOCK_AMOUNT = 25;
+var uploadField = document.querySelector('.img-upload__input');
+var editingForm = document.querySelector('.img-upload__overlay');
+var editingCloseButton = editingForm.querySelector('.img-upload__cancel');
+var zoomStep = 25;
 
 var commentTexts = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
 var commentAuthors = ['Порфирий', 'Цифровой олень', 'Куртка Бэйна', 'Владимир Ясно Солнышко', 'Наталья Орейро', 'Пушистый друг', 'Последний абориген Полинезии', 'Коля, просто Коля', 'Мама Стифлера'];
+
+var MOCK_AMOUNT = 25;
+var ESC_KEY = 27;
 
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -71,3 +74,36 @@ for (var i = 0; i < MOCK_AMOUNT; i++) {
 }
 
 photoContainer.appendChild(fragment);
+
+var imgUploadHandler = function () {
+  editingForm.classList.remove('hidden');
+
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEY) {
+      editingForm.classList.add('hidden');
+    }
+  });
+};
+
+var closeButtonClickHandler = function () {
+  editingForm.classList.add('hidden');
+};
+
+uploadField.addEventListener('change', imgUploadHandler);
+
+editingCloseButton.addEventListener('click', closeButtonClickHandler);
+
+var zoomInButton = editingForm.querySelector('.scale__control--bigger');
+var zoomOutButton = editingForm.querySelector('.scale__control--smaller');
+var scaleInput = editingForm.querySelector('.scale__control--value');
+// var scaleDefaultValue = 55;
+
+zoomInButton.addEventListener('click', function () {
+  scaleInput.value = parseInt(scaleInput.value, 10) + zoomStep + '%';
+});
+
+zoomOutButton.addEventListener('click', function () {
+  scaleInput.value = parseInt(scaleInput.value, 10) - zoomStep + '%';
+});
+
+// debugger
