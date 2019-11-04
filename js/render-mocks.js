4a7f41b13ace6c075fb21ef31dc1d2f43d26f7ca
booolat <50450347+booolat@users.noreply.github.com>
2019-11-04 15:59:38 +0300

    Модуль загрузки моков

--- js/render-mocks.js
+++ js/render-mocks.js
@@ -5,16 +5,26 @@
   var photoContainer = document.querySelector('.pictures');
   var mockTemplate = document.querySelector('#picture').content;
   var fragment = document.createDocumentFragment();
+  var mockError = document.querySelector('#error').content;
 
-  for (var i = 0; i < window.mocks.MOCK_AMOUNT; i++) {
-    var currentMock = window.mocks.mockArray[i];
-    var photoMock = mockTemplate.cloneNode(true);
-    photoMock.querySelector('.picture__img').setAttribute('src', currentMock.url);
-    photoMock.querySelector('.picture__likes').textContent = currentMock.likes;
-    photoMock.querySelector('.picture__comments').textContent = currentMock.comments.length;
-    fragment.appendChild(photoMock);
-  }
+  var successHandler = function (webmock) {
 
-  photoContainer.appendChild(fragment);
+    for (var i = 0; i < webmock.length; i++) {
+      var currentMock = webmock[i];
+      var photoMock = mockTemplate.cloneNode(true);
+      photoMock.querySelector('.picture__img').setAttribute('src', currentMock.url);
+      photoMock.querySelector('.picture__likes').textContent = currentMock.likes;
+      photoMock.querySelector('.picture__comments').textContent = currentMock.comments.length;
+      fragment.appendChild(photoMock);
+    }
 
+    photoContainer.appendChild(fragment);
+  };
+
+  var errorHandler = function (errorMessage) {
+    // console.log(errorMessage);
+    photoContainer.appendChild(mockError);
+  };
+
+  window.load(successHandler, errorHandler);
 })();
